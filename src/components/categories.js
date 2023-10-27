260482b83b2f6d77beb03e0c819865dad462c9dc
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-10-27 18:01:50 +0200

    Categories first commit

---
+++ src/components/categories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './categories.css'; // Import the CSS file
+
+const Categories = () => {
+  return (
+    <div className="categories-container"> {/* Apply the class for the container */}
+      <p className="categories-header">Categories</p>
+    </div>
+  );
+};
+
+export default Categories;
