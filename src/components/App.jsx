30b0070f5565d8d6ae863c412c2ab51c5d1227fd
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-10-27 20:01:01 +0200

    bugfix/browserouter and toastify

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import WelcomePage from 'pages/WelcomePage/WelcomePage';
+import RegisterPage from 'pages/RegisterPage/RegisterPage';
+import SigninPage from 'pages/SigninPage/SigninPage';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<WelcomePage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/signin" element={<SigninPage />} />
     </Routes>
   );
 };
