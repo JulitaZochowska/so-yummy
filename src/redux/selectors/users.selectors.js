6171ffeaf0bf60ef500c3e9c7018f26c11360e57
tojaarek <119487863+tojaarek@users.noreply.github.com>
2023-10-28 19:16:15 +0200

    feature/selectors added

--- src/redux/selectors/users.selectors.js
+++ src/redux/selectors/users.selectors.js
@@ -1,1 +1,3 @@
 export const selectRegistered = state => state.users.isRegistered;
+export const selectIsLoggedIn = state => state.users.isLoggedIn;
+export const selectisRefreshing = state => state.users.isRefreshing;
