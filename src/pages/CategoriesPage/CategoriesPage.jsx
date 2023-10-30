981212301fc7bbae0dd61968dd8def6da246ae2f
Rafal Chojnacki <r.chojnacki2@o2.pl>
2023-10-30 21:40:54 +0100

    Added background elements

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -1,6 +1,11 @@
 import { Footer } from 'components/Footer/Footer';
 import { Header } from 'components/Header/Header';
 import css from './CategoriesPage.module.css';
+import ImageGallery from 'components/Categories/ImageGallery';
+import spinachImage from 'images/spinach-leafs.png';
+import rectangle8 from 'images/rectangle8.png';
+import rectangle9 from 'images/rectangle9.png';
+import rectangle11 from 'images/rectangle11.png';
 import { element } from 'prop-types';
 const CategoriesPage = () => {
   return (
@@ -21,8 +26,26 @@ const CategoriesPage = () => {
           <h2 className={css.CategoriesPage_nav_h2}>Seafood</h2>
           <h2 className={css.CategoriesPage_nav_h2}>Side</h2>
         </nav>
+        <div>
+          <ImageGallery />
+        </div>
+        <div className={css.rectangle8}>
+        <img src={rectangle8} alt="Black rectangle" />
+        </div>
+        <div className={css.rectangle9}>
+        <img src={rectangle9} alt="Green rectangle" />
+        </div>
+        <div className={css.rectangle11}>
+        <img src={rectangle11} alt="Green rectangle" />
+        </div>
+        <div className={css.spinachImage}>
+        <img src={spinachImage} alt="Spinach Leaves" />
+        </div>
       </div>
+      <div>
       <Footer />
+      </div>
+      
     </div>
   );
 };
