3a785f7ecab1c3aa15dca7dc39d4c04bab6ed099
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-11-02 22:15:38 +0100

    Remove unused files

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -3,10 +3,6 @@ import { Header } from 'components/Header/Header';
 import css from './CategoriesPage.module.css';
 import CategoriesGalleryImage from 'components/Categories/CategoriesGallery';
 import CategoriesList from 'components/CategoriesList/CategoriesList'; // Corrected import
-import spinachImage from 'images/spinach-leafs.png';
-import rectangle8 from 'images/rectangle8.png';
-import rectangle9 from 'images/rectangle9.png';
-import rectangle11 from 'images/rectangle11.png';
 
 const CategoriesPage = () => {
   return (
