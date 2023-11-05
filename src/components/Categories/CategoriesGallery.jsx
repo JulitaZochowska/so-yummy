23d48bd479505eda4b408db1c651fc7b5001712a
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-11-05 20:58:01 +0100

    Change of number of meals displayed for one page

--- src/components/Categories/CategoriesGallery.jsx
+++ src/components/Categories/CategoriesGallery.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import css from './CategoriesGallery.module.css';
 import RecipeTile from 'components/RecipeTile/RecipeTile';
@@ -11,33 +11,62 @@ const CategoriesGalleryImage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
-    dispatch(fetchCategoriesList());
+    dispatch(fetchCategoriesList())
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error loading data', error);
+        setLoading(false);
+      });
   }, [dispatch]);
 
   const currentCategory = useSelector(selectSelectedCategoryName);
 
   useEffect(() => {
-    dispatch(fetchCategoryRecipes(currentCategory));
+    setLoading(true);
+    dispatch(fetchCategoryRecipes(currentCategory))
+      .then(() => {
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error loading data', error);
+        setLoading(false);
+      });
   }, [dispatch, currentCategory]);
 
   const recipes = useSelector(
-    state => state.recipes.categories[currentCategory]
+    (state) => state.recipes.categories[currentCategory]
   );
+
+  const itemsPerPage = 8;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const startIdx = (currentPage - 1) * itemsPerPage;
+  const endIdx = currentPage * itemsPerPage;
+
+  const displayedRecipes = recipes && recipes.length > 0 ? recipes.slice(startIdx, endIdx) : [];
+
+  const loadMore = () => {
+    setCurrentPage(currentPage + 1);
+  };
 
   return (
     <div className={css.recipesGrid}>
-      {recipes && recipes.length > 0 ? (
-        recipes.map(recipe => (
-          <RecipeTile
-            className={css.recipeTile}
-            key={recipe._id}
-            recipe={recipe}
-            onClick={() => navigate('/recipe/' + recipe._id)}
-          ></RecipeTile>
-        ))
-      ) : (
-        <Loader/>
+      {displayedRecipes.map((recipe) => (
+        <RecipeTile
+          className={css.recipeTile}
+          key={recipe._id}
+          recipe={recipe}
+          onClick={() => navigate('/recipe/' + recipe._id)}
+        />
+      ))}
+      {loading && <Loader />}
+      {recipes && recipes.length > endIdx && !loading && (
+        <button onClick={loadMore}>Load More</button>
       )}
     </div>
   );
