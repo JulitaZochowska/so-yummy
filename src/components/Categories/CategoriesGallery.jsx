a24f8fa59fb2ff952c5fbe099994924ebbdcbd26
Rafał Chojnacki <r.chojnacki2@o2.pl>
2023-11-05 18:11:30 +0100

    Added gallery for beef category

--- src/components/Categories/CategoriesGallery.jsx
+++ src/components/Categories/CategoriesGallery.jsx
@@ -1,44 +1,45 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import css from 'pages/CategoriesPage/CategoriesPage.module.css';
+import css from './CategoriesGallery.module.css';
 import RecipeTile from 'components/RecipeTile/RecipeTile';
-import {
-  fetchCategoryRecipes,
-  fetchCategoriesList,
-} from 'redux/actions/recipes.actions';
+import { fetchCategoryRecipes, fetchCategoriesList } from 'redux/actions/recipes.actions';
 import { selectSelectedCategoryName } from 'redux/selectors/recipes.selectors';
 import { useNavigate } from 'react-router-dom';
 
-const CategoriesGalleryImage = _ => {
+const CategoriesGalleryImage = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  useEffect(() => {
+    dispatch(fetchCategoriesList());
+  }, [dispatch]);
+
   const currentCategory = useSelector(selectSelectedCategoryName);
-  console.log(currentCategory);
 
   useEffect(() => {
     dispatch(fetchCategoryRecipes(currentCategory));
-  }, [dispatch]);
+  }, [dispatch, currentCategory]);
 
   const recipes = useSelector(
     state => state.recipes.categories[currentCategory]
   );
-  console.log(recipes);
 
-  // return (
-  //   <div>
-  //     <ul className={css.recipesLine}>
-  //       {recipes.map(recipe => (
-  //         <RecipeTile
-  //           className={css.recipeTile}
-  //           key={recipe._id}
-  //           recipe={recipe}
-  //           onClick={() => navigate('/recipe/' + recipe._id)}
-  //         ></RecipeTile>
-  //       ))}
-  //     </ul>
-  //   </div>
-  // );
+  return (
+    <div className={css.recipesGrid}>
+      {recipes && recipes.length > 0 ? (
+        recipes.map(recipe => (
+          <RecipeTile
+            className={css.recipeTile}
+            key={recipe._id}
+            recipe={recipe}
+            onClick={() => navigate('/recipe/' + recipe._id)}
+          ></RecipeTile>
+        ))
+      ) : (
+        <p>No recipes available for the selected category.</p>
+      )}
+    </div>
+  );
 };
 
 export default CategoriesGalleryImage;
